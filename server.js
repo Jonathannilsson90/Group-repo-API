95ce08c2a6a41a4f77fb795d6a8f9bc82bb3ef89
Jonathan Nilsson <koffeinmissbrukarn@gmail.com>
2023-01-23 15:11:02 +0100

    Updated-server.js-middleware

--- server.js
+++ server.js
@@ -1,16 +1,37 @@
+/// NPM package Declatation
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
-const port = process.env.PORT || 3000;
-const app = express();
-//midle weare
 
+const app = express();
+/// .env processes
+const port = process.env.PORT || 5000;
+
+//Middleware declaclaration 
+const helmet = require('helmet')
+const cors = require('cors')
+const jwt = require('jsonwebtoken')
+
+
+///Middleware start-ups
+app.use(helmet());
+app.use(cors());
 app.use(express.json());
 
-const bookRoutes = require("./routes/bookRoutes");
-app.use("/books", bookRoutes);
+/// Routing for books and user(s) with verifcation
 
-//start server
+const bookRoutes = require("./routes/bookRoutes");
+app.use("/books", verificationToken, bookRoutes);
+
+const userRoutes = require('./routes/userRoutes')
+app.use('/users', userRoutes)
+
+///Server start-up with mongoose connection and localhost
+
+mongoose.set('strictQuery', false);
+mongoose.connect(process.env.DB_URL, () => console.log("Server is now connected to the DB"))
+
+
 app.listen(port, () => {
-  console.log(`Listening to port: ${port}`);
+  console.log(`Server running on local-port: ${port}`);
 });
